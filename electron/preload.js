5c9fc7a991fd8a4af01901039a719de0a217e6c3
Rolzon <soporte@grupo-rz.lat>
2025-11-17 23:11:48 -0500

    Correcciones

--- electron/preload.js
+++
@@ -1,7 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('electron', {
-  // Add any electron-specific APIs here if needed
-  platform: process.platform,
-  versions: process.versions,
-});
